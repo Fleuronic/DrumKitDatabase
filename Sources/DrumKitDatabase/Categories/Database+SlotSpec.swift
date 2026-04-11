066fbaa6540b6538797e96872c73b9eafa4bbc88
Jordan Kay <jordan@fleuronic.com>
2026-04-11 10:35:26 -0400

    Update slot

--- Sources/DrumKitDatabase/Categories/Database+SlotSpec.swift
+++ Sources/DrumKitDatabase/Categories/Database+SlotSpec.swift
@@ -7,7 +7,6 @@ import protocol Catenoid.Fields
 import protocol DrumKitService.SlotSpec
 
 extension Database: SlotSpec where SlotSpecifiedFields: Decodable {
-	public func listSlots(in year: Int) async -> Results<SlotSpecifiedFields> {
-		await fetch(where: Slot.Identified.predicate(year: year))
-	}
+	public typealias SlotList = Results<SlotSpecifiedFields>
+	public typealias SlotFetch = SingleResult<SlotSpecifiedFields?>
 }
